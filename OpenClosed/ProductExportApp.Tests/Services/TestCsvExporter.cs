55f94ef11e320664d8eb83c54628f7a113b740d1
Marcelo Muñoz <marcelo.munoz@jalasoft.com>
2025-05-26 21:04:16 -0400

    YamlFormat and Test Result

--- OpenClosed/ProductExportApp.Tests/Services/TestCsvExporter.cs
+++ OpenClosed/ProductExportApp.Tests/Services/TestCsvExporter.cs
@@ -29,6 +29,100 @@ public class TestCsvExporter
 
     Assert.Equal(expected, result);
   }
-  
+
   // TODO: Add tests for the other exporter types.
+  [Fact]
+  public void Export_ProducesXmlFormatedString()
+  {
+    var exporter = new XmlExporter();
+
+    var result = exporter.Export(_products);
+
+    var expected = """
+    <Products>
+      <Product id="1">
+        <Name>Laptop</Name>
+        <Price>7999.99</Price>
+        <Category>Electronics</Category>
+      </Product>
+      <Product id="2">
+        <Name>Desk</Name>
+        <Price>4999.99</Price>
+        <Category>Furniture</Category>
+      </Product>
+      <Product id="3">
+        <Name>Fork</Name>
+        <Price>2999.99</Price>
+        <Category>Kitchen</Category>
+      </Product>
+    </Products>
+
+    """;
+
+    Assert.Equal(expected, result);
+  }
+
+  [Fact]
+  public void Export_ProducesJsonFormatedString()
+  {
+    var exporter = new JsonExporter();
+
+    var result = exporter.Export(_products);
+
+    var expected = """
+    [
+      {
+        "Id": 1,
+        "Name": "Laptop",
+        "Price": 7999.99,
+        "Category": "Electronics"
+      },
+      {
+        "Id": 2,
+        "Name": "Desk",
+        "Price": 4999.99,
+        "Category": "Furniture"
+      },
+      {
+        "Id": 3,
+        "Name": "Fork",
+        "Price": 2999.99,
+        "Category": "Kitchen"
+      }
+    ]
+    """;
+
+    Assert.Equal(expected, result);
+  }
+
+  [Fact]
+  public void Export_ProducesYamlFormatedString()
+  {
+    var exporter = new YamlExporter();
+
+    var result = exporter.Export(_products);
+
+    var expected = """
+    Products:
+      - Product:
+        Id: "1"
+        Name: "Laptop"
+        Price: "7999.99"
+        Category: "Electronics"
+      - Product:
+        Id: "2"
+        Name: "Desk"
+        Price: "4999.99"
+        Category: "Furniture"
+      - Product:
+        Id: "3"
+        Name: "Fork"
+        Price: "2999.99"
+        Category: "Kitchen"
+
+    """;
+
+    Assert.Equal(expected, result);
+  }
+
 }
